9aa41414900a7828fee4d26af8d1d140a4c1da70
gferreira <gustavo@hipertipo.com>
2023-09-20 08:58:28 -0300

    improving script to copy icons from fontra repo

--- _py/collect-icons.py
+++ _py/collect-icons.py
@@ -1,20 +1,27 @@
-import os, glob, shutil
+import os, json, glob
+from urllib.request import urlopen
 
-# update these to match your system
-fontraFolder = '/Users/gferreira/hipertipo/tools/fontra'
-fontraDocsIconsFolder = '/Users/gferreira/hipertipo/tools/fontra-docs/images/icons'
+# directory contents in json form
+iconsFontraURL = 'https://api.github.com/repos/googlefonts/fontra/contents/src/fontra/client/images'
+iconsTablerURL = 'https://api.github.com/repos/googlefonts/fontra/contents/src/fontra/client/tabler-icons'
 
-iconsFolder  = os.path.join(fontraFolder, 'src/fontra/client/images')
-tablerFolder = os.path.join(fontraFolder, 'src/fontra/client/tabler-icons')
+# target docs folder for icon images
+baseFolder = os.path.dirname(os.getcwd())
+iconsFolder = os.path.join(baseFolder, 'images', 'icons')
 
-assert os.path.exists(iconsFolder)
-assert os.path.exists(tablerFolder)
+# clear existing files
+# for f in glob.glob(f'{iconsFolder}/*.*'):
+#     os.remove(f)
 
-iconsFontra = glob.glob(f'{iconsFolder}/*.svg')
-iconsTabler = glob.glob(f'{tablerFolder}/*.svg')
-
-iconsAll = iconsFontra + iconsTabler
-
-for svgPath in iconsAll:
-    dstPath = os.path.join(fontraDocsIconsFolder, os.path.split(svgPath)[-1])
-    shutil.copyfile(svgPath, dstPath)
+# copy icons from repository to icons folder
+for URL in [iconsFontraURL, iconsTablerURL]:
+    response = urlopen(URL)
+    data = json.loads(response.read())
+    for item in data:
+        dstPath = os.path.join(iconsFolder, item['name'])
+        # download from URL
+        with urlopen(item['download_url']) as file:
+            content = file.read()
+        # save to file
+        with open(dstPath, 'wb') as download:
+            download.write(content)
